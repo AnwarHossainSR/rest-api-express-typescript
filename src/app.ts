543bd5b73c60308d7cb796c315ab7b90decb0273
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-14 15:58:41 +0600

    setup logger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
+import config from "config";
 import dotenv from "dotenv";
 import express from "express";
 import connect from "./utils/connect";
+import logger from "./utils/logger";
+const port = config.get<number>("port");
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(4000, async () => {
-    console.log("Server is running on port 4000");
-    await connect();
+app.listen(port, async () => {
+  logger.info(`App is running at http://localhost:${port}`);
+  await connect();
 });
