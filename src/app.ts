b3adaba3b4ce9c626230267c983151c77e01efc1
AnwarHossainSR <mahedianwar@gmail.com>
2022-06-16 21:55:40 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import config from "config";
 import dotenv from "dotenv";
 import express from "express";
+import responseTime from "response-time";
 import deserializeUser from "./middleware/deserializeUser";
 import routes from "./routes";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import { restResponseTimeHistogram, startMetricsServer } from "./utils/metrics";
 const port = config.get<number>("port");
 dotenv.config();
 
@@ -13,8 +15,24 @@ const app = express();
 app.use(express.json());
 app.use(deserializeUser);
 
+app.use(
+  responseTime((req: Request, res: Response, time: number) => {
+    if (req?.route?.path) {
+      restResponseTimeHistogram.observe(
+        {
+          method: req.method,
+          route: req.route.path,
+          status_code: res.statusCode,
+        },
+        time * 1000
+      );
+    }
+  })
+);
+
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`);
   await connect();
+  startMetricsServer();
   routes(app);
 });
